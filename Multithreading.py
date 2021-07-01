53b4c6e645089cb2103fdf281fc2677ed5459425
mormic21 <mormichi4@gmail.com>
2021-07-01 17:13:32 +0200

    Multiprocessing

--- Multithreading.py
+++ Multithreading.py
@@ -2,23 +2,32 @@ import threading
 import time
 
 class myFred(threading.Thread):
+    Ergebnis = [0, 1]
+
     def __init__(self, id, name):
         threading.Thread.__init__(self)
         self.id = id
         self.name = name
 
     def run(self):
-        lockMe.acquire()
-        print("Starte" + str(self.id))
-        time.sleep(self.id * 3)
-        lockMe.release()
-        print("Ende" + str(self.id))
+        i = 0
+        while i < 20:
+            lockMe.acquire()
+            zahl = myFred.Ergebnis[len(myFred.Ergebnis) - 2] + myFred.Ergebnis[len(myFred.Ergebnis) - 1]
+            myFred.Ergebnis.append(zahl)
+            lockMe.release()
+            i = i + 1
+
+
+
+        #lockMe.acquire()
+        #print("Starte" + str(self.id))
+        #time.sleep(self.id * 3)
+        #lockMe.release()
+        #print("Ende" + str(self.id))
 
 if __name__ == '__main__':
-
     lockMe = threading.Lock()
-
-
     t1 = myFred(1, "t1")
     t2 = myFred(2, "t2")
 
@@ -26,6 +35,10 @@ if __name__ == '__main__':
     t2.start()
     #main geht erst weiter, wenn der jeweilige thread beendet ist
     t1.join()
+    #while-schleife anstatt join
+   # while t1.isAlive():
+       # time.sleep(1)
     t2.join()
 
-    print("Ende des Main-Threads")
+    print(myFred.Ergebnis)
+   # print("Ende des Main-Threads")
